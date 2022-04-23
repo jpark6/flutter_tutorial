db20495af0dbf74af02634430155d2d524fbde26
jpark6 <qkstjr09@gmail.com>
2022-04-23 16:16:45 +0900

    Feat: Apply Infinite Scroll

--- lib/main.dart
+++ lib/main.dart
@@ -10,32 +10,51 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
+    final wordPair = WordPair.random();  // DELETE
+
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Startup Name Generator'),
         ),
-        body: const Center(
-          child: RandomWords(),
+        body: const Center(                     // Add the const
+          //child: Text(wordPair.asPascalCase), // REPLACE with...
+          child: RandomWords(),                 // ...this line
         ),
       ),
     );
   }
 }
-
 class RandomWords extends StatefulWidget {
   const RandomWords({Key? key}) : super(key: key);
 
   @override
-  RandomWordsState createState() => RandomWordsState();
+  State<RandomWords> createState() => _RandomWordsState();
 }
 
-class RandomWordsState extends State<RandomWords> {
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
-  }
-}
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+    );
+  }}
